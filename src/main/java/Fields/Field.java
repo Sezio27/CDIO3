99a0fadf412de4675eaada29c5ae2d2c845f20b6
Aisha <aimoh21@student.sdu.dk>
2022-11-25 15:53:28 +0100

    arv

--- src/main/java/Fields/Field.java
+++ src/main/java/Fields/Field.java
@@ -2,12 +2,7 @@ package Fields;
 
 import gui_fields.GUI_Field;
 import spil.Player;
-/* Arv er en fundamental objekt-orienteret designteknik, der bruges til at skabe og organisere genanvendelige klasser.
-En abstrakt klasse repræsenterer et generelt koncept i et klassehierarki. En abstrakt klasse kan ikke instansieres.
-Hvis alle fieldklasser har en landOnField metode der gør noget forskelligt,
-så kan child klasserne override metoden fra parent klassen,
-hvor de alle har den samme signatur som i parent klassen, men de har så hver især en anderledes body, hvor de gør noget forskelligt.
-*/
+
 public abstract class Field {
 
     String name;
